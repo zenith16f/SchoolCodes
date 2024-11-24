2e385b52e14da1ebfe32794bf62d63a1f806da91
zenith16f <fguzmanc2100@alumno.ipn.mx>
2024-11-23 18:45:14 -0600

    From text to array of dom and codom done

--- Discrete Mathematics/AnalizadorFunciones.c
+++ Discrete Mathematics/AnalizadorFunciones.c
@@ -1,6 +1,6 @@
 // Defines
-#define EXPRESSION 100
 #define MAXSetSize 64
+#define MAXGraficaSize 128
 
 // Includes
 #include <stdbool.h>
@@ -19,7 +19,7 @@
 // Functions
 // Funcion que verifica si es una funcion usando dominio, codominio y grafica
 void CondicionFuncion(unsigned int *dominio, unsigned int *codominio,
-			     unsigned int *grafica) {
+			     unsigned int *grafica,bool *cumple) {
   // Variables
 
   // Verificacion
@@ -27,7 +27,8 @@ void CondicionFuncion(unsigned int *dominio, unsigned int *codominio,
 };
 
 // Funcion que recibe la funcion y verifica si es inyectiva
-void CondicionInyectividad(char *funcion) {
+void CondicionInyectividad(unsigned int *dominio, unsigned int *codominio,
+			     unsigned int *grafica, bool *cumple) {
   // Variables
 
   // Verificacion
@@ -35,7 +36,8 @@ void CondicionInyectividad(char *funcion) {
 };
 
 // Funcion que recibe la funcion y verifica si es suprayectiva
-void CondicionSuprayectividad(char *funcion) {
+void CondicionSuprayectividad(unsigned int *dominio, unsigned int *codominio,
+			       unsigned int *grafica, bool *cumple) {
   // Variables
 
   // Verificacion
@@ -43,16 +45,17 @@ void CondicionSuprayectividad(char *funcion) {
 };
 
 // Funcion que recibe la funcion y verifica si es biyectiva
-void CondicionBiyectividad(char *funcion) {
+void CondicionBiyectividad(unsigned int *dominio, unsigned int *codominio,
+			    unsigned int *grafica) {
   // Variables
-  bool condicionInicial, cumpleInyectividad, cumpleSuprayectividad;
+  bool cumpleFuncion, cumpleInyectividad, cumpleSuprayectividad;
 
   // Main Condition
-  
+  CondicionFuncion(dominio, codominio, grafica, &cumpleFuncion);
 
-  while (condicionInicial) {
-    CondicionInyectividad(funcion);
-    CondicionSuprayectividad(funcion);
+  while (cumpleFuncion) {
+    CondicionInyectividad(dominio, codominio, grafica, &cumpleInyectividad);
+    CondicionSuprayectividad(dominio, codominio, grafica, &cumpleSuprayectividad);
 
     if (cumpleInyectividad && cumpleSuprayectividad) {
       printf("La funcion cumple con las condiciones de inyectividad y "
@@ -65,7 +68,7 @@ void CondicionBiyectividad(char *funcion) {
     }
   }
 
-  if (!condicionInicial) {
+  if (!cumpleFuncion) {
     printf("La expresion no es una funcion por lo que no cumple la condicion "
            "de suprayectividad e inyectividad\n");
     printf("Por lo tanto no es biyectiva\n");
@@ -73,8 +76,30 @@ void CondicionBiyectividad(char *funcion) {
 };
 
 // Funcion que recibe un conjunto en texto y lo transforma en un conjunto de
-// elementos
-void SetConjunto(char *conjunto) {};
+// elementos (Array)
+int *SetConjunto(char *conjunto, int *numeroElementos) {
+	// Variables
+	static int arregloConjunto[MAXSetSize];
+	int i = 0;
+	char *ptr = strtok(conjunto, ",");
+
+	// Transformacion
+	while (ptr != NULL && i < MAXSetSize) {
+		arregloConjunto[i++] = atoi(ptr);
+		ptr = strtok(NULL, ",");
+	}
+
+	// Return 
+	*numeroElementos = i;
+	return arregloConjunto;
+};
+
+// Funcion que recibe un conjunto de elementos y lo transforma en un conjunto de tuplas (Array)
+void graficaConjunto(char *conjunto) {
+	// Variables
+	unsigned int conjuntoEx;
+
+};
 
 // Funcion que recibe la grafica de una funcion y la transforma en un conjunto
 // de elementos
@@ -83,8 +108,8 @@ void SetGrafica(char *grafica) {};
 // Main Function
 int main(void) {
   // Variables
-  char expresion[EXPRESSION], dominioSTR[MAXSetSize], codominioSTR[MAXSetSize];
-  int dominio[MAXSetSize], codominio[MAXSetSize], grafica[MAXSetSize];
+  char dominioSTR[MAXSetSize], codominioSTR[MAXSetSize], graficaSTR[MAXGraficaSize];
+  int numeroElementosDom, numeroElementosCodom, numeroElementosGraf;
 
   // Bienvenida
   printf("Este programa se encarga de recibir una expresion.\n");
@@ -92,21 +117,40 @@ int main(void) {
          "funcion, en caso de ser funcion si es inyectiva, suprayectiva y/o "
          "biyectiva en base a su dominio y codominio.\n");
 
-  // Expression Input
-  ExpresionAlgebraica(expresion);
-
   // Dom and Codom Input
-
-  printf("Ingrese el dominio de la funcion con comas (Elementos maximos 64 en "
+  printf("Ingrese el dominio de la funcion con comas (Elementos maximos 32, en "
          "los enteros positivos): ");
   scanf("%s", dominioSTR);
+  int *dominio = SetConjunto(dominioSTR, &numeroElementosDom);
 
-  printf("Ingrese el codominio de la funcion con comas (Elementos maximos 64 "
+  printf("Ingrese el codominio de la funcion con comas (Elementos maximos 32, "
          "en los enteros positivos): ");
   scanf(" %s", codominioSTR);
+  int *codominio = SetConjunto(codominioSTR, &numeroElementosCodom);
 
-  printf("El dominio de la funcion es: %s\n", dominioSTR);
-  printf("El codominio de la funcion es: %s\n", codominioSTR);
+  // Grafica Input (Se separa de par en par)
+  printf("Ingrese la grafica de la funcion con comas (Elementos maximos 64, en "
+	   "los enteros positivos): ");
+  scanf(" %s", graficaSTR);
+
+  printf("El dominio de la funcion en txt es: %s\n", dominioSTR);
+  printf("El codominio de la funcion en txt es: %s\n", codominioSTR);
+
+ printf("El dominio (arreglo) contiene %d elementos:\n", numeroElementosDom);
+    for (int i = 0; i < numeroElementosDom; i++) {
+        printf("%d ", dominio[i]);
+    }
+    printf("\n");
+
+    printf("El codominio (arreglo) contiene %d elementos:\n", numeroElementosCodom);
+    for (int i = 0; i < numeroElementosCodom; i++) {
+	  printf("%d ", codominio[i]);
+    }
+    printf("\n");
+
+  printf("La grafica de la funcion es: %s\n", graficaSTR);
+
+
 
   // Biyectividad Call (This function call the other functions)
   // CondicionBiyectividad(expresion);
